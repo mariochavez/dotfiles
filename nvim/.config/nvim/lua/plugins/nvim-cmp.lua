6b74dd2990fe4b295154bb6357afcf8b6fc45fa2
Mario Alberto Chávez <mario.chavez@gmail.com>
2024-07-20 13:14:38 -0600

    Adds supermaven

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,10 +1,10 @@
 return {
-	"hrsh7th/nvim-cmp",
-	opts = function(_, opts)
-		local cmp = require("cmp")
-		opts.window = {
-			completion = cmp.config.window.bordered(),
-			documentation = cmp.config.window.bordered(),
-		}
-	end,
+  "hrsh7th/nvim-cmp",
+  opts = function(_, opts)
+    local cmp = require("cmp")
+    opts.window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+    }
+  end,
 }
