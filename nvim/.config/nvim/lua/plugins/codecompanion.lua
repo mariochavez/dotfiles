11f18da753f6a1db1b8f9a9a9efd19bd18b8f026
Mario Alberto Chávez <mario.chavez@gmail.com>
2025-04-26 11:36:57 -0600

    Update starship config

--- nvim/.config/nvim/lua/plugins/codecompanion.lua
+++ nvim/.config/nvim/lua/plugins/codecompanion.lua
@@ -1,9 +1,35 @@
---- Ollama config for CodeCompanion.
+-- Choose default model from a list of preferred models. Fall back to first
+-- available.
+local function preferred_model_picker(preferred)
+  return function(self)
+    if self == nil then
+      -- Debug:render() calls Adapter.schema.model.default() but doesn't pass
+      -- in `self`. Just return an empty string in this case.
+      return ""
+    end
+
+    local choices = self.schema.model.choices(self)
+
+    for _, best in ipairs(preferred) do
+      for _, choice in ipairs(choices) do
+        if choice:find(best, 1, true) then
+          return choice
+        end
+      end
+    end
+
+    return choices[1]
+  end
+end
+
 local ollama_fn = function()
   return require("codecompanion.adapters").extend("ollama", {
     schema = {
       model = {
-        default = "qwen2.5-coder:7b",
+        default = preferred_model_picker({
+          "qwen2.5-coder:7b",
+          "qwen2.5-coder:32b",
+        }),
         -- default = "llama3.1:8b",
         -- default = "codellama:7b",
       },
@@ -24,7 +50,6 @@ end
 local supported_adapters = {
   -- anthropic = anthropic_fn,
   -- openai = openai_fn,
-  -- gemini = gemini_fn,
   ollama = ollama_fn,
 }
 
@@ -177,7 +202,7 @@ return {
             },
           },
           inline = {
-            adapter = "ollama",
+            adapter = "ollama_q7",
           },
         },
         display = {
